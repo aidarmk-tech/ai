0ac7eac17c7ea2b6fe0acc153594837960ff6e6b
aidarmk-tech <aidar.m.k@gmail.com>
2026-07-25 10:59:49 +0600

    Bump database schema for 3 percent outcomes

--- pumpradar/app/src/main/java/com/aidar/pumpradar/data/local/PumpRadarDatabase.kt
+++ pumpradar/app/src/main/java/com/aidar/pumpradar/data/local/PumpRadarDatabase.kt
@@ -10,7 +10,7 @@ import androidx.room.RoomDatabase
         SignalTrajectoryEntity::class, ShadowSignalEntity::class,
         SnapshotOutcomeEntity::class
     ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 abstract class PumpRadarDatabase : RoomDatabase() {
