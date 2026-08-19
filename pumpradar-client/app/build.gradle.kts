9da85ee1e16803c3cfecd615c5c3360d1fee19f8
aidarmk-tech <aidar.m.k@gmail.com>
2026-08-20 01:18:36 +0500

    Bump PumpRadar client to 1.3.1

--- pumpradar-client/app/build.gradle.kts
+++ pumpradar-client/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.aidar.pumpradar.client"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.1"
     }
 
     buildTypes {
