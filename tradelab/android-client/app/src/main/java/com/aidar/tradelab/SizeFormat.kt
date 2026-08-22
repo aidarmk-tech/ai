47a08f2294c542bd5726903394962388fcc7f7f7
aidarmk-tech <aidar.m.k@gmail.com>
2026-08-22 13:51:34 +0500

    Show small snapshot sizes in KB

---
+++ tradelab/android-client/app/src/main/java/com/aidar/tradelab/SizeFormat.kt
@@ -0,0 +1,9 @@
+package com.aidar.tradelab
+
+fun formatBytes(bytes: Long): String {
+    if (bytes < 1024) return "$bytes B"
+    val kb = bytes / 1024.0
+    if (kb < 1024) return "%.1f KB".format(kb)
+    val mb = kb / 1024.0
+    return "%.1f MB".format(mb)
+}
