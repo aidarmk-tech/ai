4cddd54fa05cf365dc7d18624bb81b927a940b9e
aidarmk-tech <aidar.m.k@gmail.com>
2026-08-22 20:59:18 +0500

    Bump Android client to 0.2.5

--- tradelab/android-client/app/build.gradle.kts
+++ tradelab/android-client/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aidar.tradelab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.2.4"
+        versionCode = 7
+        versionName = "0.2.5"
         val serverUrl = System.getenv("TRADELAB_SERVER_URL") ?: "https://45.150.37.187"
         buildConfigField("String", "SERVER_URL", "\"$serverUrl\"")
     }
